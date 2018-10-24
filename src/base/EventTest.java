1b6c094ac9ce8083dc000b603f279c9b90459a7c
xiezhiping <1529464401@qq.com>
2018-10-24 15:25:56 +0800

    添加：java中事件相关基础知识的练习类

---
+++ src/base/EventTest.java
@@ -0,0 +1,14 @@
+/**
+ * java中事件相关基础知识的练习类
+ * 特别说明：如果后期涉及类比较多，考虑重新涉及包结构，这里只用于某些不成体系的单个知识点细节练习
+ */
+package base;
+
+public class EventTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
