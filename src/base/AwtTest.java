b2167ac485ccbc6f733e5694b75fe1383980fc25
xiezhiping <1529464401@qq.com>
2018-10-24 11:40:48 +0800

    test

---
+++ src/base/AwtTest.java
@@ -0,0 +1,18 @@
+/**
+ * AWT相关基础练习测试类：用于查看一些底层实现
+ */
+package base;
+
+import java.awt.Frame;
+
+public class AwtTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Frame frame = new Frame("测试窗口");
+		frame.setBounds(30,20,250,200);
+		frame.setVisible(true);
+
+	}
+
+}
