d95044f4ba123c931e08f0304cd597cc5b704517
xiezhiping <1529464401@qq.com>
2018-10-29 21:44:59 +0800

    bak

--- src/base/multithreading/SleepThreadTest.java
+++ src/base/multithreading/SleepThreadTest.java
@@ -11,7 +11,6 @@ public class SleepThreadTest {
 			try {
 				Thread.sleep(1000);
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
