b68477cb9010598216e2dafc8b9cff6250208717
xiezhiping <1529464401@qq.com>
2018-10-29 21:44:09 +0800

    sleep线程：即使系统空闲也不执行

---
+++ src/base/multithreading/SleepThreadTest.java
@@ -0,0 +1,21 @@
+package base.multithreading;
+
+import java.util.Date;
+
+public class SleepThreadTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < 10; i++) {
+			System.out.println("当前时间：" + new Date());
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
