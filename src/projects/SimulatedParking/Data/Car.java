482169d9a6d89dbfd15f31295cf014dc2a7bc8df
xiezhiping <1529464401@qq.com>
2019-01-14 19:19:10 +0800

    bak

---
+++ src/projects/SimulatedParking/Data/Car.java
@@ -0,0 +1,5 @@
+package projects.SimulatedParking.Data;
+
+public class Car {
+	
+}
---
+++ src/projects/SimulatedParking/test/Test.java
@@ -0,0 +1,16 @@
+package projects.SimulatedParking.test;
+
+import projects.SimulatedParking.utils.Util;
+
+/**
+ * 整个项目的测试类
+ * @author XZP
+ *
+ */
+public class Test {
+
+	public static void main(String[] args) {
+		Util.Println(Util.getPalte());
+	}
+
+}
