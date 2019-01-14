12f53e5e41d841702858c5b7177af221207d01f9
xiezhiping <1529464401@qq.com>
2019-01-14 19:27:46 +0800

    添加涉及的一些类，明天再接着写

--- src/projects/SimulatedParking/Data/Car.java
+++ src/projects/SimulatedParking/Data/Car.java
@@ -1,5 +1,16 @@
 package projects.SimulatedParking.Data;
 
+import projects.SimulatedParking.utils.Util;
+
 public class Car {
+	private String plate;
+	public Car() {
+		this.plate = Util.getPalte(); // 每辆对应一个牌照id
+	}
+	
+	// getter
+	public String getPlate() {
+		return plate;
+	}
 	
 }
---
+++ src/projects/SimulatedParking/Data/Parking.java
@@ -0,0 +1,17 @@
+package projects.SimulatedParking.Data;
+
+public class Parking {
+	private String name;
+	public Parking(String name) {
+		this.name = name;
+	}
+	
+	// getter and setter
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
