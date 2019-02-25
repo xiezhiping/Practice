646daeb7f140d1140dc220d133566af3446637f2
xiezhiping <1529464401@qq.com>
2019-02-25 17:45:04 +0800

    一个字符串处理、文件I/O的小DEMO，用来预处理数据分析的数据

---
+++ src/base/IO/predata/Main.java
@@ -0,0 +1,37 @@
+package base.IO.predata;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		File file = new File("F:\\java_workspace\\Practice\\src\\base\\IO\\predata\\resource\\data.txt");
+		FileReader fd = new FileReader(file);
+		BufferedReader br = new BufferedReader(fd);
+		String s = "";
+		StringBuilder sb = new StringBuilder();
+		String sub1 = "";
+		String sub2 = "";
+		while((s = br.readLine()) != null) {
+			sub1 = s.split(":")[0];
+			sub2 = s.split(":")[1];
+			if ("front".equals(sub2) == false) {
+				sub2 = "front";
+			}
+			sb.append(sub1 + ":" + sub2 + "\n");
+		}
+		System.out.println(sb.toString());
+		File f = new File("F:\\java_workspace\\Practice\\src\\base\\IO\\predata\\resource\\result.txt");
+		FileWriter fw = new FileWriter(f);
+		BufferedWriter bw = new BufferedWriter(fw);
+		bw.write(sb.toString());
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+}
